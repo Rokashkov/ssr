b068cb0da7a39bc6fd2d8ce7d7aa629073ef6dfe
Vladimir <vovalol31@gmail.com>
2022-08-30 15:43:46 +0300

    deploy

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+
+const __dist = path.resolve(__dirname, '/..', '/..', '/dist')
+
+app.use(express.static(path.resolve(__dist)))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dist, '/index.html'))
+})
+
+export default app
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import http from 'http'
+import https from 'https'
+import fs from 'fs'
+import path from 'path'
+import redirect from './redirect'
+import app from './app'
+
+const __certs = path.resolve(__dirname, '/..', '/..', '/..', '/etc', '/letsencrypt', '/live', '/rokashkov.ru')
+const privateKey  = fs.readFileSync(path.resolve(__certs, '/privkey.pem'), 'utf8')
+const certificate = fs.readFileSync(path.resolve(__certs, '/fullchain.pem'), 'utf8')
+const credentials = {
+	key: privateKey,
+	cert: certificate
+}
+
+http.createServer(redirect).listen(80)
+https.createServer(credentials, app).listen(443)
