e83185e0ae2f549892d0b6a217c82b7809ccf400
Vladimir <vovalol31@gmail.com>
2022-09-04 15:26:13 +0300

    eg

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import path from 'path'
 
 const app = express()
 
-const __dist = path.resolve(__dirname, '/..', '/..', '/dist')
+const __dist = path.resolve(__dirname, '..', '..', 'dist')
 
 app.use(express.static(path.resolve(__dist)))
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dist, '/project-name', '/index.html'))
+	res.sendFile(path.resolve(__dist, 'project-name', 'index.html'))
 })
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ import redirect from './redirect'
 import app from './app'
 
 const __certs = path.resolve(__dirname, '..', '..', '..', 'etc', 'letsencrypt', 'live', 'rokashkov.ru')
-console.log(__dirname)
-console.log(__certs)
+
 const privateKey  = fs.readFileSync(path.resolve(__certs, 'privkey.pem'), 'utf8')
 const certificate = fs.readFileSync(path.resolve(__certs, 'fullchain.pem'), 'utf8')
+
 const credentials = {
 	key: privateKey,
 	cert: certificate
