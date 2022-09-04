13882a9afabe7aca01e5fa79f42222abd094fe73
Vladimir <vovalol31@gmail.com>
2022-09-04 14:46:27 +0300

    1

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const __dist = path.resolve(__dirname, '/..', '/..', '/dist')
 app.use(express.static(path.resolve(__dist)))
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dist, '/index.html'))
+	res.sendFile(path.resolve(__dist, 'project-name', '/index.html'))
 })
 
 export default app
